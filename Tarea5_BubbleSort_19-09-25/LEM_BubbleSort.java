2c56b38f3755c3b8c89e07043b57f51d95d6fa9f
Luis-Eduardo-Manzanarez <uasluedma@gmail.com>
2025-09-21 22:02:18 -0700

    Update LEM_BubbleSort.java

--- Tarea5_BubbleSort_19-09-25/LEM_BubbleSort.java
+++ Tarea5_BubbleSort_19-09-25/LEM_BubbleSort.java
@@ -1,34 +1,45 @@
+import java.util.Random;
+
 public class Main {
-    
-    public static void bubbleSort(int[] arr, int size) {
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = 0; j < size - i - 1; j++) {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            boolean swap = false;
+            for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swap = true;
                 }
+            }
+            if (!swap) {
+                break;
             }
         }
     }
 
-    public static void mostrarArreglo(int[] arr, int size) {
-        for (int i = 0; i < size; i++) {
-            System.out.print(arr[i] + " ");
+    public static void main(String[] args) {
+        final int SIZE = 10;
+        int[] arr = new int[SIZE];
+        Random rand = new Random();
+
+        for (int i = 0; i < SIZE; i++) {
+            arr[i] = rand.nextInt(100) + 1;
+        }
+
+        System.out.print("Arreglo original: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
         System.out.println();
-    }
 
-    public static void main(String[] args) {
-        int[] arr = {21, 13, 44, 32, 78, 2};
-        int size = arr.length;
+        bubbleSort(arr);
 
-        System.out.print("Lista original: ");
-        mostrarArreglo(arr, size);
-
-        bubbleSort(arr, size);
-
-        System.out.print("Lista ordenada: ");
-        mostrarArreglo(arr, size);
+        System.out.print("Arreglo ordenado: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
